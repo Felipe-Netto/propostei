c7add91ae19b941b0cb4f11ec271ff799ffb0d0f
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2026-06-16 18:55:39 -0300

    fix(security): mantém dados sensíveis no env

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:5173', credentials: true });
+  app.enableCors({ origin: process.env.URL_WEB, credentials: true });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getAccessToken } from '@/storage/auth-storage'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 api.interceptors.request.use((config) => {
